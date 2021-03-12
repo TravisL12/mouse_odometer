38ed94ab60fc61be3a8b230b94420887ef8fd750
Travis Lawrence <travis.lawrence12@gmail.com>
2021-03-12 13:29:26 -0800

    We are in business

--- public/scripts/background.js
+++
@@ -1,8 +0,0 @@
-function loadScript(srcUrl) {
-  const script = document.createElement('script');
-  script.src = chrome.extension.getURL(srcUrl);
-  console.log(srcUrl, 'srcUrl srcUrl');
-  (document.head || document.documentElement).appendChild(script);
-}
-loadScript('./public/scripts/odometer.min.js');
-loadScript('./public/scripts/application.js');
